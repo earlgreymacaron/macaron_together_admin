0471994196d9b2e40d2704491176a4a2273ee8b0
macaron <yeaseul.parky@gmail.com>
2017-01-20 15:52:41 +0900

    Update ServiceMonitor.java

--- app/src/main/java/com/cs496/macaron_together_admin/ServiceMonitor.java
+++ app/src/main/java/com/cs496/macaron_together_admin/ServiceMonitor.java
@@ -13,37 +13,31 @@ import android.util.Log;
 import java.util.List;
 
 public class ServiceMonitor extends Service {
-
     /**
      * {@link Log#isLoggable(String, int)}
      * <p>
      * IllegalArgumentException is thrown if the tag.length() > 23.
      */
-    private static final String TAG = "NotifiCollectorMonitor";
+    private static final String TAG = "ServiceMonitor";
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d(TAG, "onCreate() called");
+        Log.d(TAG, "Service Monitor onCreate() called");
 
         ComponentName cc = new ComponentName(this, NotificationListener.class);
-        Log.v(TAG, "checkListening collectorComponent: " + cc);
+        Log.v(TAG, "collectorComponent: " + cc);
         ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
         boolean listening = false;
         List<ActivityManager.RunningServiceInfo> runningServices
                 = manager.getRunningServices(Integer.MAX_VALUE);
-        if (runningServices == null ) {
-            Log.w(TAG, "checkListening runningServices is NULL");
+        if (runningServices == null) {
+            Log.w(TAG, "runningServices is NULL");
             return;
         }
         for (ActivityManager.RunningServiceInfo service : runningServices) {
             if (service.service.equals(cc)) {
-                Log.w(TAG, "checkListening service - pid: " + service.pid +
-                        ", currentPID: " + Process.myPid() +
-                        ", clientPackage: " + service.clientPackage +
-                        ", clientCount: " + service.clientCount
-                        + ", clientLabel: " + ((service.clientLabel == 0) ? "0" : "(" + getResources().getString(service.clientLabel) + ")"));
-                if (service.pid == Process.myPid() /*&& service.clientCount > 0 && !TextUtils.isEmpty(service.clientPackage)*/) {
+                if (service.pid == Process.myPid()) {
                     listening = true;
                 }
             }
